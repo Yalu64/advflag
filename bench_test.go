1d13270e1a9010e8b8de32d09d6a548c3dbc312e
Yalu <56490576+Yalu64@users.noreply.github.com>
2022-09-18 23:59:54 +0200

    Update bench_test.go

--- bench_test.go
+++ bench_test.go
@@ -6,13 +6,13 @@ import (
 	"testing"
 )
 
-var argAdvFlag1 = advflag.String("arg1", "this_is_an_test_value", nil)
-var argAdvFlag2 = advflag.String("arg2", "this_is_an_test_value", nil)
-var argAdvFlag3 = advflag.String("arg3", "this_is_an_test_value", nil)
+var argAdvFlag1 = advflag.String("arg1", "this_is_a_test_value", nil)
+var argAdvFlag2 = advflag.String("arg2", "this_is_a_test_value", nil)
+var argAdvFlag3 = advflag.String("arg3", "this_is_a_test_value", nil)
 
-var argGoFlag1 = flag.String("arg1", "this_is_an_test_value", "")
-var argGoFlag2 = flag.String("arg2", "this_is_an_test_value", "")
-var argGoFlag3 = flag.String("arg3", "this_is_an_test_value", "")
+var argGoFlag1 = flag.String("arg1", "this_is_a_test_value", "")
+var argGoFlag2 = flag.String("arg2", "this_is_a_test_value", "")
+var argGoFlag3 = flag.String("arg3", "this_is_a_test_value", "")
 
 func BenchmarkAdvFlag(b *testing.B) {
 	for x := 0; x < b.N; x++ {
